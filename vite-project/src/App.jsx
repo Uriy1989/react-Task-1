b920923c68832d35827974eb60e41c096983a881
Shevsov Yuriy <yuri-admi.nistrator@yandex.ru>
2026-02-13 22:29:07 +0700

    without using JSX

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,38 +8,54 @@ import { createElement } from 'react';
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    //сам компонент реакт это декларативный стиль
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        //callback функция императивный стииль
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        //callback функция императивный стииль
-        {(() => {
-          const currentYear = new Date().getFullYear();
-          return currentYear;
-        })()}
-      </div>
-    </>
+  const handlerClick = () => {
+    setCount(count + 1);
+  };
+
+  const currentYear = new Date().getFullYear();
+
+   return createElement(
+    'div',
+    null,
+
+    createElement(
+      'div',
+      null,
+      createElement(
+        'a',
+        { href: 'https://vite.dev', target: '_blank' },
+        createElement('img', { src: viteLogo, className: 'logo', alt: 'Vite logo' })
+      ),
+      createElement(
+        'a',
+        { href: 'https://react.dev', target: '_blank' },
+        createElement('img', { src: reactLogo, className: 'logo react', alt: 'React logo' })
+      )
+    ),
+
+    createElement('h1', null, 'Vite + React'),
+
+    createElement(
+      'div',
+      { className: 'card' },
+      createElement('button', { onClick: handlerClick }, `count is ${count}`)
+    ),
+
+    createElement(
+      'p',
+      null,
+      'Edit ',
+      createElement('code', null, 'src/App.jsx'),
+      ' and save to test HMR'
+    ),
+
+    createElement(
+      'p',
+      { className: 'read-the-docs' },
+      'Click on the Vite and React logos to learn more'
+    ),
+
+    createElement('div', null, currentYear)
   )
 }
 
