3bb4d8dc75668675f784aa8c924390500a1ea113
Shevsov Yuriy <yuri-admi.nistrator@yandex.ru>
2026-02-13 22:22:32 +0700

    comment code style

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createElement } from 'react';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    //сам компонент реакт это декларативный стиль
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -18,6 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        //callback функция императивный стииль
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -29,6 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <div>
+        //callback функция императивный стииль
         {(() => {
           const currentYear = new Date().getFullYear();
           return currentYear;
